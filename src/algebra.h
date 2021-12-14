7d22279a356bc51aa878b3de223f75596743bbc4
ghsk1 <Mauricio.291202@gmail.com>
2021-12-14 12:15:52 -0600

    Init commit

--- src/algebra.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-double algeb_sum(double val1, double val2);
-double algeb_subs(double val1, double val2);
-double algeb_divs(double val1, double val2);
-double algeb_mult(double val1, double val2);
-double algeb_power(double base, double exponent);
-double algeb_square(double x, double n);
